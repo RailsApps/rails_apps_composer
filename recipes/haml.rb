a185a9714e97ecc328b2b4734bd053dd76675aef
Daniel Kehoe <kehoe@fortuity.com>
2011-04-12 16:01:40 -0700

    replace haml recipe

--- recipes/haml.rb
+++ recipes/haml.rb
@@ -1,11 +1,14 @@
-gem 'haml', '>= 3.0.0'
-gem 'haml-rails'
+# Application template recipe for the rails3_devise_wizard. Check for a newer version here:
+# https://github.com/fortuity/rails3_devise_wizard/blob/master/recipes/haml.rb
+
+gem 'haml', '>= 3.0.25'
+gem 'haml-rails', '>= 0.3.4', :group => :development
 
 __END__
 
 name: HAML
 description: "Utilize HAML for templating."
-author: mbleigh
+author: fortuity
 
 category: templating
 exclusive: templating
