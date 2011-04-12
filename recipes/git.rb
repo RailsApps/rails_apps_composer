7c32c26199196da70405f45085c2b582bcfbdaae
Daniel Kehoe <kehoe@fortuity.com>
2011-04-12 16:01:14 -0700

    repalce git recipe

--- recipes/git.rb
+++ recipes/git.rb
@@ -1,14 +1,25 @@
+# Application template recipe for the rails3_devise_wizard. Check for a newer version here:
+# https://github.com/fortuity/rails3_devise_wizard/blob/master/recipes/git.rb
+
 after_everything do
+  # Git should ignore some files
+  remove_file '.gitignore'
+  get "https://github.com/fortuity/rails3-gitignore/raw/master/gitignore.txt", ".gitignore"
+  # Initialize new Git repo
   git :init
   git :add => '.'
-  git :commit => '-m "Initial import."'
+  git :commit => "-aqm 'Initial commit of a new Rails app'"
+  # Create a git branch
+  git :checkout => ' -b working_branch'
+  git :add => '.'
+  git :commit => "-m 'Initial commit of working_branch'"
 end
 
 __END__
 
 name: Git
 description: "Provides basic Git setup for the Rails app and commits the initial repository."
-author: mbleigh
+author: fortuity
 
 exclusive: scm
 category: other
