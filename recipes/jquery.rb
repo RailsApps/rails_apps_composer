a4125899dcabb6d1e7e8ea39a0d0d1c1eab0db64
Daniel Kehoe <kehoe@fortuity.com>
2011-04-12 16:02:26 -0700

    replace jquery recipe

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -1,15 +1,36 @@
-gem 'jquery-rails'
+# Application template recipe for the rails3_devise_wizard. Check for a newer version here:
+# https://github.com/fortuity/rails3_devise_wizard/blob/master/recipes/jquery.rb
 
 after_bundler do
-  ui = config['ui'] ? ' --ui' : ''
-  generate "jquery:install#{ui}"
+  # remove the Prototype adapter file
+  remove_file 'public/javascripts/rails.js'
+  # remove the Prototype files (if they exist)
+  remove_file 'public/javascripts/controls.js'
+  remove_file 'public/javascripts/dragdrop.js'
+  remove_file 'public/javascripts/effects.js'
+  remove_file 'public/javascripts/prototype.js'
+  # add jQuery files
+  inside "public/javascripts" do
+    get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "rails.js"
+    get "https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js", "jquery.js"
+    if config['ui']
+      get "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js", "jqueryui.js"
+    end
+  end
+  # adjust the Javascript defaults
+  if config['ui']
+    inject_into_file 'config/application.rb', "config.action_view.javascript_expansions[:defaults] = %w(jquery jqueryui rails)\n", :after => "config.action_view.javascript_expansions[:defaults] = %w()\n", :verbose => false
+  else
+    inject_into_file 'config/application.rb', "config.action_view.javascript_expansions[:defaults] = %w(jquery rails)\n", :after => "config.action_view.javascript_expansions[:defaults] = %w()\n", :verbose => false
+  end  
+  gsub_file "config/application.rb", /config.action_view.javascript_expansions\[:defaults\] = \%w\(\)\n/, ""
 end
 
 __END__
 
 name: jQuery
 description: "Adds the latest jQuery and Rails UJS helpers for jQuery."
-author: mbleigh
+author: fortuity
 
 exclusive: javascript_framework
 category: assets
