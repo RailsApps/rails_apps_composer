558f624fe3029914e290d30b249c5ebaa620f57c
Daniel Kehoe <kehoe@fortuity.com>
2011-04-22 17:54:11 -0700

    update seed_database recipe to accommodate ActiveRecord

--- recipes/seed_database.rb
+++ recipes/seed_database.rb
@@ -11,13 +11,18 @@ after_bundler do
     append_file 'db/seeds.rb' do <<-FILE
 puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user.name
 FILE
     end
   end
 
+  # create a default user
+  append_file 'db/seeds.rb' do <<-FILE
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user.name
+FILE
+  end
+      
   run 'rake db:seed'
 
 end
