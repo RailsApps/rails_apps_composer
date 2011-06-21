d9627798162fbedb04576ea21564198902b0d41e
Daniel Kehoe <kehoe@fortuity.com>
2011-06-21 13:09:19 -0700

    update gem 'cucumber-rails' from 0.5.2 to 1.0.0

--- recipes/cucumber.rb
+++ recipes/cucumber.rb
@@ -10,7 +10,7 @@ if config['cucumber']
     gem 'launchy', '0.4.0', :group => :test
   else
     # for Rails 3.1+, use optimistic versioning for gems
-    gem 'cucumber-rails', '>= 0.5.2', :group => :test
+    gem 'cucumber-rails', '>= 1.0.0', :group => :test
     gem 'capybara', '>= 1.0.0', :group => :test
     gem 'database_cleaner', '>= 0.6.7', :group => :test
     gem 'launchy', '>= 0.4.0', :group => :test
