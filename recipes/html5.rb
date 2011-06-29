e3ce72312c5d7f5e99b13402316faebef54c87c2
Daniel Kehoe <kehoe@fortuity.com>
2011-06-29 15:26:14 -0700

    tweak app layout for html5 recipe

--- recipes/html5.rb
+++ recipes/html5.rb
@@ -76,7 +76,7 @@ RUBY
   %head
     %title #{app_name}
     %meta{:charset => "utf-8"}
-    %meta{:http-equiv => "X-UA-Compatible", :content => "IE=edge,chrome=1"}
+    %meta{"http-equiv" => "X-UA-Compatible", :content => "IE=edge,chrome=1"}
     %meta{:name => "viewport", :content => "width=device-width, initial-scale=1, maximum-scale=1"}
     = stylesheet_link_tag :application
     = javascript_include_tag :application
