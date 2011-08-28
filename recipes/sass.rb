c108c70422898a3e313560689e4e578dc6e08407
Ashley Woodard <ashley.c.woodard@gmail.com>
2011-08-28 01:43:45 -0500

    add option to use sass syntax instead of scss by default

--- recipes/sass.rb
+++ recipes/sass.rb
@@ -1,11 +1,25 @@
 gem 'sass', '>= 3.1.6'
 
+if config['sass']
+  after_bundler do
+    create_file 'config/initializers/sass.rb' do <<-RUBY
+Rails.application.config.generators.stylesheet_engine = :sass
+RUBY
+    end
+  end
+end
+
 __END__
 
 name: SASS
 description: "Utilize SASS for really awesome stylesheets!"
-author: mbleigh & mrc2407
+author: mbleigh, mrc2407, & ashley_woodard
 
 exclusive: css_replacement
 category: assets
 tags: [css]
+
+config:
+  - sass:
+      type: boolean
+      prompt: Would you like to use SASS syntax instead of SCSS?
