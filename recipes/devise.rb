520a4b57269ea11e0e053ba226a0daabadf2d4ad
Daniel Kehoe <daniel@danielkehoe.com>
2011-08-29 12:55:18 -0700

    improve fix for Devise/Cucumber issue, update mongoid from 2.1.9 to 2.2.0

--- recipes/devise.rb
+++ recipes/devise.rb
@@ -37,7 +37,7 @@ if config['devise']
     if recipes.include? 'cucumber'
       # Cucumber wants to test GET requests not DELETE requests for destroy_user_session_path
       # (see https://github.com/RailsApps/rails3-devise-rspec-cucumber/issues/3)
-      gsub_file 'config/initializers/devise.rb', 'config.sign_out_via = :delete', 'config.sign_out_via = :get'
+      gsub_file 'config/initializers/devise.rb', 'config.sign_out_via = :delete', 'config.sign_out_via = Rails.env.test? ? :get : :delete'
     end
     
   end
