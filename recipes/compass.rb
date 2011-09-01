204b61e566fe9db2940a885487392abefcf0393d
Ashley Woodard <ashley.c.woodard@gmail.com>
2011-08-31 21:28:47 -0500

    add recipe for Compass (with alpha support for rails 3.1)

---
+++ recipes/compass.rb
@@ -0,0 +1,42 @@
+if config['compass']
+  if recipes.include? 'rails 3.1'
+    gem 'compass', :version => '~> 0.12.alpha.0'
+
+    after_bundler do
+      remove_file 'app/assets/stylesheets/application.css'
+      create_file 'app/assets/stylesheets/application.css.sass' do <<-SASS
+//= require_self
+//= require_tree . 
+
+@import compass
+@import _blueprint
+SASS
+      end
+    end
+  else
+    gem 'compass', :version => '~> 0.11'
+
+    after_bundler do
+      run 'compass init rails'
+    end
+  end
+else
+  receipes.delete('compass')
+end
+
+__END__
+
+name: Compass
+description: "Utilize Compass framework for SASS."
+author: ashley_woodard
+
+requires: [sass]
+run_after: [sass]
+exclusive: css_framework
+category: assets
+tags: [css]
+
+config:
+  - compass:
+      type: boolean
+      prompt: Would you like to use Compass for stylesheets?
