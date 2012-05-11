282a5e5db74dbb5a9b1ba835c434a2d28093da21
Daniel Kehoe <daniel@danielkehoe.com>
2012-05-11 13:39:04 -0700

    fix 'cucumber' recipe to add ':require => false' and avoid warning message

--- recipes/cucumber.rb
+++ recipes/cucumber.rb
@@ -2,7 +2,7 @@
 # https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/cucumber.rb
 
 if config['cucumber']
-  gem 'cucumber-rails', '>= 1.3.0', :group => :test
+  gem 'cucumber-rails', '>= 1.3.0', :group => :test, :require => false
   gem 'capybara', '>= 1.1.2', :group => :test
   gem 'database_cleaner', '>= 0.7.2', :group => :test
   gem 'launchy', '>= 2.1.0', :group => :test
