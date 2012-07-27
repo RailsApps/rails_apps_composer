01613a6f66f76c96faa0e53a23a9202a3bde2559
Akiva Levy <akiva@sixthirteendesign.com>
2012-07-27 12:28:38 -0700

    Removing section on README file from cleanup recipe as it's inappropriate for the recipe, decoupled into separate README recipe

--- recipes/cleanup.rb
+++ recipes/cleanup.rb
@@ -10,14 +10,9 @@ after_bundler do
     README
     doc/README_FOR_APP
     public/index.html
+    public/favicon.ico
     app/assets/images/rails.png
   }.each { |file| remove_file file }
-
-  # add placeholder READMEs
-  get "https://raw.github.com/RailsApps/rails3-application-templates/master/files/sample_readme.txt", "README"
-  get "https://raw.github.com/RailsApps/rails3-application-templates/master/files/sample_readme.textile", "README.textile"
-  gsub_file "README", /App_Name/, "#{app_name.humanize.titleize}"
-  gsub_file "README.textile", /App_Name/, "#{app_name.humanize.titleize}"
 
   # remove commented lines and multiple blank lines from Gemfile
   # thanks to https://github.com/perfectline/template-bucket/blob/master/cleanup.rb
@@ -27,7 +22,7 @@ after_bundler do
   # remove commented lines and multiple blank lines from config/routes.rb
   gsub_file 'config/routes.rb', /  #.*\n/, "\n"
   gsub_file 'config/routes.rb', /\n^\s*\n/, "\n"
-  
+
 end
 
 __END__
