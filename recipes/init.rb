127ed8025d4db2b3232628d0434d3bc2d79251c5
Vitaly Kushner <vitaly@astrails.com>
2012-08-22 22:50:45 +0300

    fix user model when using devise
    :confirmable devise parameter was missing

--- recipes/init.rb
+++ recipes/init.rb
@@ -9,9 +9,11 @@ after_everything do
       ## DEVISE-CONFIRMABLE
       append_file 'db/seeds.rb' do <<-FILE
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => Time.now.utc
+user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user.confirm!
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => Time.now.utc
+user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user2.confirm!
 puts 'New user created: ' << user2.name
 FILE
       end
