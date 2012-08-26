RailsApps/rails_apps_composer

commit 1cf28c09cc4a8d11f2f1a7e5b91fce724d0a9b73

support supplying gems in defaults file

lib/rails_wizard/command.rb
@@ -13,7 +13,7 @@ module RailsWizard
       recipes, defaults = load_defaults
       args = ask_for_args(defaults)
       recipes = ask_for_recipes(recipes)
-      gems = ask_for_gems
+      gems = ask_for_gems(defaults)
       run_template(name, recipes, gems, args, defaults, nil)
     end
 
@@ -99,8 +99,8 @@ module RailsWizard
         recipes
       end
 
-      def ask_for_gems
-        gems = []
+      def ask_for_gems(defaults)
+        gems = defaults["gems"] || []
         while getgem = ask("#{bold}What gem would you like to add? #{clear}#{yellow}(blank to finish)#{clear}")
           if getgem == ''
             break

commit 778615c0b291dc39d5fedccde016cefc5297e45a

fix regression introduced by 1cf28c09cc4a8d11f2f1a7e5b91fce724d0a9b73 (version 2.1.10)

lib/rails_wizard/command.rb
@@ -25,7 +25,7 @@ module RailsWizard
       add_recipes
       recipes, defaults = load_defaults
       recipes = ask_for_recipes(recipes)
-      gems = ask_for_gems
+      gems = ask_for_gems(defaults)
       run_template(nil, recipes, gems, nil, defaults, template_name)
     end
 