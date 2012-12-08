e75b93b989d610c343853140c1afb374eeab7dbc
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-08 12:41:34 -0800

    update 'saas' recipe to add Rspec and Cucumber tests

--- recipes/saas.rb
+++ recipes/saas.rb
@@ -27,6 +27,7 @@ if prefer :railsapps, 'rails-stripe-membership-saas'
     # >-------------------------------[ Cucumber ]--------------------------------<
     say_wizard "copying Cucumber scenarios from the rails-stripe-membership-saas examples"
     remove_file 'features/users/user_show.feature'
+    copy_from_repo 'features/support/paths.rb', :repo => repo
     copy_from_repo 'features/users/sign_in.feature', :repo => repo
     copy_from_repo 'features/users/sign_up.feature', :repo => repo
     copy_from_repo 'features/users/sign_up_with_stripe.feature', :repo => repo
