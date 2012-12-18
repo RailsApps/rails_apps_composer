496de91478351344af3b07262f62df7612d9b1a2
Daniel Kehoe <daniel@danielkehoe.com>
2012-12-17 17:42:55 -0800

    use 'find_or_create_by_email' instead of 'find_or_create_by_name'

--- recipes/init.rb
+++ recipes/init.rb
@@ -72,7 +72,7 @@ FILE
   unless prefer :authentication, 'omniauth'
     append_file 'db/seeds.rb' do <<-FILE
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_name :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 FILE
     end
