39111d7f42a05ce5d768e166d64c8dffa4f51e67
David Van Der Beek <earlynovrock@gmail.com>
2012-12-29 10:08:20 -0600

    add deployment recipe

---
+++ recipes/deployment.rb
@@ -0,0 +1,43 @@
+after_everything do
+	run "ln -s #{destination_root} ~/.pow/#{app_name}"
+	
+	gsub_file 'Gemfile', /gem 'sqlite3'\n/, ''
+	gem 'pg', :group => :production
+	gem 'sqlite3', :group => [:development, :test]
+
+	gsub_file 'config/application.rb', /config.assets.enabled = true/ do
+	<<-RUBY
+config.assets.enabled = true
+	config.assets.initialize_on_precompile=false
+RUBY
+	end
+
+	%w{
+    public/index.html
+    app/assets/images/rails.png
+  }.each { |file| remove_file file }
+
+	run "heroku create"
+	run "rake assets:precompile"
+	git :add => "."
+	git :commit => "-a -m 'initial commit for heroku'"
+	run "git push heroku master"
+	
+	# SET ENVIRONMENT VARIABLES BEFORE SEEDING DB
+	run "heroku config:add 'ROLES=[admin, user, VIP]'"
+  run "heroku config:add ADMIN_NAME='First User' ADMIN_EMAIL='user@example.com' ADMIN_PASSWORD='please'"
+  run "heroku config"
+
+  run "heroku run rake db:migrate"
+	run "heroku run rake db:seed"
+end
+
+__END__
+
+name: deployment
+description: "Set up POW and delpoy to Heroku"
+author: David
+
+requires: []
+run_after: [init]
+category: deployment
