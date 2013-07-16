610a91ed17939fbd26218fc813278071a4fef404
Mark D. Blackwell <markdblackwell01@gmail.com>
2013-07-16 10:34:23 -0400

    Add megatest, along with mega Rake commands

---
+++ megatest/config/rails_stripe_membership_saas_testing_recipe.rb
@@ -0,0 +1,20 @@
+prefs[:ban_spiders] = false
+prefs[:continuous_testing] = 'none'
+prefs[:dev_webserver] = 'webrick'
+prefs[:drop_database] = true
+prefs[:github] = false
+prefs[:jsruntime] = false
+prefs[:prod_webserver] = 'same'
+prefs[:rvmrc] = false
+prefs[:templates] = 'erb'
+
+
+
+prefs[:gems] = %w[ selenium-webdriver ]
+prefs[:railsapps] = 'rails-stripe-membership-saas'
+say_wizard "selected rails-stripe-membership-saas testing recipe"
+__END__
+name:                rails_stripe_membership_saas_testing_recipe
+description:         rails-stripe-membership-saas testing recipe
+author: RailsApps
+category: testing
