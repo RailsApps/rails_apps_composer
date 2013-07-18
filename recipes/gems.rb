8cac174879513f83ad6295ab3691bd4b243e99ab
Jon Conley <conley.jj@gmail.com>
2013-07-18 17:06:29 -0600

    Remove assets group in gem file for rails 4

--- recipes/gems.rb
+++ recipes/gems.rb
@@ -11,6 +11,8 @@ insert_into_file('Gemfile', "ruby '#{RUBY_VERSION}'\n", :before => /^ *gem 'rail
 gsub_file 'Gemfile', /group :doc do/, ''
 gsub_file 'Gemfile', /\s*gem 'sdoc', require: false\nend/, ''
 
+assets_group = Rails::VERSION::MAJOR.to_s == "4" ? nil : :assets
+
 ## Web Server
 if (prefs[:dev_webserver] == prefs[:prod_webserver])
   add_gem 'thin' if prefer :dev_webserver, 'thin'
@@ -90,14 +92,14 @@ add_gem 'machinist', :group => :test if prefer :fixtures, 'machinist'
 
 ## Front-end Framework
 add_gem 'bootstrap-sass' if prefer :bootstrap, 'sass'
-add_gem 'compass-rails', :group => :assets if prefer :frontend, 'foundation'
-add_gem 'zurb-foundation', :group => :assets if prefer :frontend, 'foundation'
+add_gem 'compass-rails', :group => assets_group if prefer :frontend, 'foundation'
+add_gem 'zurb-foundation', :group => assets_group if prefer :frontend, 'foundation'
 if prefer :bootstrap, 'less'
-  add_gem 'less-rails', :group => :assets
-  add_gem 'twitter-bootstrap-rails', :group => :assets
+  add_gem 'less-rails', :group => assets_group
+  add_gem 'twitter-bootstrap-rails', :group => assets_group
   # install gem 'therubyracer' to use Less
   add_gem 'libv8'
-  add_gem 'therubyracer', :group => :assets, :platform => :ruby, :require => 'v8'
+  add_gem 'therubyracer', :group => assets_group, :platform => :ruby, :require => 'v8'
 end
 
 ## Email
