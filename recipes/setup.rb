164f89901b7dfb0fbc9d63faa533f0d3a3fa7164
Daniel Kehoe <daniel@danielkehoe.com>
2013-08-26 11:23:57 -0700

    eliminate Devise Invitable module as an option for Rails 4 (closes #243)

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -89,8 +89,14 @@ if recipes.include? 'models'
       if prefer :orm, 'mongoid'
         prefs[:devise_modules] = multiple_choice "Devise modules?", [["Devise with default modules","default"]] unless prefs.has_key? :devise_modules
       else
-        prefs[:devise_modules] = multiple_choice "Devise modules?", [["Devise with default modules","default"], ["Devise with Confirmable module","confirmable"],
+        if rails_4?
+          prefs[:devise_modules] = multiple_choice "Devise modules?", [["Devise with default modules","default"],
+          ["Devise with Confirmable module","confirmable"],
           ["Devise with Confirmable and Invitable modules","invitable"]] unless prefs.has_key? :devise_modules
+        else
+          prefs[:devise_modules] = multiple_choice "Devise modules?", [["Devise with default modules","default"],
+          ["Devise with Confirmable module","confirmable"]] unless prefs.has_key? :devise_modules
+        end
       end
     when 'omniauth'
       prefs[:omniauth_provider] = multiple_choice "OmniAuth provider?", [["Facebook", "facebook"], ["Twitter", "twitter"], ["GitHub", "github"],
