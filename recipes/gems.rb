54f04de85755d0a24fe75aea16d635f68134e836
Daniel Kehoe <daniel@danielkehoe.com>
2013-12-08 20:48:10 -0800

    use Zurb Foundation 4 for 'learn-rails' example

--- recipes/gems.rb
+++ recipes/gems.rb
@@ -108,7 +108,7 @@ case prefs[:frontend]
   when 'foundation4'
     if rails_4?
       add_gem 'zurb-foundation', '~> 4.3.2'
-      add_gem 'compass-rails', '~> 2.0.alpha.0'
+      add_gem 'compass-rails', '~> 1.1.2'
     else
       add_gem 'zurb-foundation', '~> 4.3.2', :group => assets_group
       add_gem 'compass-rails', '~> 1.0.3', :group => assets_group
