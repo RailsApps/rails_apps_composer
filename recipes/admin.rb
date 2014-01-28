af7102d401ac558fb226cc0c9539771e72227e2b
Steve Schwartz <steve@alfajango.com>
2014-01-28 01:35:12 -0500

    Added rails admin recipe.

---
+++ recipes/admin.rb
@@ -0,0 +1,37 @@
+case config['admin']
+  when 'activeadmin'
+    prefs[:admin] = 'activeadmin'
+  when 'rails_admin'
+    prefs[:admin] = 'rails_admin'
+end
+
+gem 'activeadmin' if prefer :admin, 'activeadmin'
+gem 'rails_admin' if prefer :admin, 'rails_admin'
+
+after_bundler do
+  if prefer :admin, 'active_admin'
+    say_wizard "recipe installing activeadmin"
+    generate 'active_admin:install'
+  end
+  if prefer :admin, 'rails_admin'
+    say_wizard "recipe installing rails_admin"
+    generate 'rails_admin:install'
+  end
+end
+
+__END__
+
+name: admin
+description: "Adding rails admin gem to your application"
+author: JangoSteve
+
+category: admin
+requires: [setup]
+run_after: [setup]
+args: -T
+
+config:
+  - admin:
+      type: multiple_choice
+      prompt: Install admin gem?
+      choices: [ ["None", "none"], ["ActiveAdmin", "activeadmin"], ["RailsAdmin", "rails_admin"] ]
