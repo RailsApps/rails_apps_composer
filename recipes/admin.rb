c602b3d98729dfbbecc47ead051142a63999a13a
Daniel Kehoe <daniel@danielkehoe.com>
2014-01-28 16:30:14 -0800

    see CHANGELOG, bump version to 2.4.22

--- recipes/admin.rb
+++ recipes/admin.rb
@@ -1,3 +1,6 @@
+# Application template recipe for the rails_apps_composer. Change the recipe here:
+# https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/admin.rb
+
 case config['admin']
   when 'activeadmin'
     prefs[:admin] = 'activeadmin'
