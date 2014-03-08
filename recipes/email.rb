3d31ab6e98ba2cb845c3595d894cbdc0bff7cc75
Daniel Kehoe <daniel@danielkehoe.com>
2014-03-07 17:32:25 -0800

    send emails in development mode (reverts #272)

--- recipes/email.rb
+++ recipes/email.rb
@@ -10,8 +10,8 @@ after_bundler do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode.
-  config.action_mailer.perform_deliveries = false
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 TEXT
       prod_email_text = <<-TEXT
   # ActionMailer Config
