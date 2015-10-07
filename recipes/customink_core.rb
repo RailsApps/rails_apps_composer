1751be26677fa6a4c0e15b32a414b8788b853632
Eduardo Ducer <eduardo.ducer@customink.com>
2015-10-07 19:52:10 -0300

    add a core recipe for customink including some of the basic gems

---
+++ recipes/customink_core.rb
@@ -0,0 +1,33 @@
+# Application template recipe for the rails_apps_composer. Change the recipe here:
+# https://github.com/RailsApps/rails_apps_composer/blob/master/recipes/core.rb
+
+## Git
+say_wizard "selected all core recipes"
+
+__END__
+
+name: core
+description: "Select all core recipes."
+author: RailsApps
+
+requires: [git, railsapps,
+  learn_rails,
+  rails_bootstrap,
+  rails_foundation,
+  rails_omniauth,
+  rails_devise,
+  rails_devise_roles,
+  rails_devise_pundit,
+  rails_signup_download,
+  rails_mailinglist_activejob,
+  rails_stripe_checkout,
+  rails_stripe_coupons,
+  rails_stripe_membership_saas,
+  setup, locale, readme, gems,
+  tests,
+  email,
+  devise, omniauth, roles,
+  frontend, pages,
+  init, analytics, deployment, extras,
+  newrelic_rpm, rollbar, dotenv]
+category: collections
