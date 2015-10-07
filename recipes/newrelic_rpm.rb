1678b65b86858fd107b8e6f1e53330dac84ef11e
Eduardo Ducer <eduardo.ducer@customink.com>
2015-10-07 19:49:43 -0300

    rename newrelic recipe to match the name of the gem

--- recipes/newrelic_rpm.rb
+++ recipes/newrelic_rpm.rb
@@ -2,16 +2,17 @@ add_gem 'newrelic_rpm'
 
 stage_two do
   say_wizard "recipe stage two"
+  #run "gem install newrelic_rpm"
   new_relic_license_key = ask_wizard("Enter New Relic license key:")
   application_name = ask_wizard("Enter application name:")
   run "bundle exec newrelic install --license_key=#{new_relic_license_key} #{application_name}"
   inject_into_file '.env', "NEW_RELIC_LICENSE_KEY=#{new_relic_license_key}\n", :before => /^\n/
-  gsub_file 'config/newrelic.yml', '^(license_key:\s?)(.*$)', "\0ENV['NEW_RELIC_LICENSE_KEY']"
+  gsub_file "config/newrelic.yml", /^(.*)(license_key:\s?)(.*$)/, "  license_key: '<%= ENV['NEW_RELIC_LICENSE_KEY'] %>'"
 end
 
 __END__
 
-name: newrelic
+name: newrelic_rpm
 description: "Add New Relic for collecting operational data"
 author: eduduc
 
