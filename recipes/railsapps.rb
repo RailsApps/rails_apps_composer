6f774a44d297e967edd782c6465b2d2b0abc3172
Daniel Kehoe <daniel@danielkehoe.com>
2015-12-19 21:28:25 +0700

    preliminary support for Rails 5.0.0.beta1

--- recipes/railsapps.rb
+++ recipes/railsapps.rb
@@ -10,6 +10,30 @@ if options[:verbose]
 end
 
 case Rails::VERSION::MAJOR.to_s
+when "5"
+  prefs[:apps4] = multiple_choice "Build a starter application?",
+    [["Build a RailsApps example application", "railsapps"],
+    ["Contributed applications (none available)", "contributed_app"],
+    ["Custom application (experimental)", "none"]] unless prefs.has_key? :apps4
+  case prefs[:apps4]
+    when 'railsapps'
+        prefs[:apps4] = multiple_choice "Choose a starter application.",
+        [["learn-rails", "learn-rails"],
+        ["rails-bootstrap", "rails-bootstrap"],
+        ["rails-foundation", "rails-foundation"],
+        ["rails-mailinglist-activejob", "rails-mailinglist-activejob"],
+        ["rails-omniauth", "rails-omniauth"],
+        ["rails-devise", "rails-devise"],
+        ["rails-devise-roles", "rails-devise-roles"],
+        ["rails-devise-pundit", "rails-devise-pundit"],
+        ["rails-signup-download", "rails-signup-download"],
+        ["rails-stripe-checkout", "rails-stripe-checkout"],
+        ["rails-stripe-coupons", "rails-stripe-coupons"],
+        ["rails-stripe-membership-saas", "rails-stripe-membership-saas"]]
+    when 'contributed_app'
+      prefs[:apps4] = multiple_choice "No contributed applications are available.",
+        [["create custom application", "railsapps"]]
+  end
 when "3"
   say_wizard "Please upgrade to Rails 4.1 or newer."
 when "4"
